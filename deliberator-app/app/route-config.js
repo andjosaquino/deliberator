401d49bed2bb987e893af1cd4c590fceed39ba76
Taylor Schoettle <tjs323@cornell.edu>
2016-01-07 17:10:26 -0700

    fixed some bugs

---
+++ deliberator-app/app/route-config.js
@@ -0,0 +1,56 @@
+/**
+ * Config
+ * @namespace Configs
+ */
+(function() {
+    'use strict';
+
+    angular
+        .module('app')
+        .config(config);
+
+    // /**
+    //  * @namespace config
+    //  * @desc Route config
+    //  * @memberOf Configs
+    //  */
+    // function config($routeProvider) {
+    //     $routeProvider
+    //         .when("/", {
+    //             templateUrl: 'detail/detail.html',
+    //             controller: 'DetailCtrl'
+    //         })
+    //         .when("/details/:candidate_id", {
+    //             templateUrl: 'detail/detail.html',
+    //             controller: 'DetailCtrl',
+    //             controllerAs: 'detail'
+    //         })
+    //         .otherwise({redirectTo: '/'});
+    // }
+
+    function config($stateProvider, $urlRouterProvider){
+      //
+      // For any unmatched url, redirect to /state1
+      $urlRouterProvider.otherwise("/selection");
+      //
+      // Now set up the states
+      $stateProvider
+        .state('deliberation', {
+          url: "/deliberation",
+          templateUrl: "list/list.html",
+          controller: 'ListCtrl',
+          controllerAs: 'list'
+        })
+        .state('deliberation.detail', {
+          url: "/detail/:candidate_id",
+          templateUrl:'detail/detail.html',
+          controller: 'DetailCtrl',
+          controllerAs: 'detail'
+        })
+        .state('overview', {
+          url: "/overview",
+          templateUrl: "overview/overview.html"
+        })
+    }
+
+})();
